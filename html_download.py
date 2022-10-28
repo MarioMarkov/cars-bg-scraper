1ae4d585f06ee2bfcda595d0b64f1c11c6d3dc5a
Mario Markov <mario11septemvri@gmail.com>
2022-10-28 03:04:48 +0300

    Downloading all possible cars

--- html_download.py
+++ html_download.py
@@ -8,13 +8,18 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 
 #TODO Dupplicates appear around 50 page index
 # #996 destinct cars
-for page in range(1,80):
-    url = 'https://www.cars.bg/carslist.php?subm=1&add_search=1&typeoffer=1&conditions%5B0%5D=4&conditions%5B1%5D=1&ajax=1&page={id}'                                                               
-    print(url.format(id = page))
-    driver.get(url.format(id = page))
-    time.sleep(1)
-    file = open('DS.html', 'a', encoding="utf-8")
-    file.write(driver.page_source)
+#https://www.cars.bg/carslist.php?subm=1&add_search=1&typeoffer=1&brandId=86&conditions%5B%5D=4&conditions%5B%5D=1&ajax=1&page=3&time=1666898830836
+for brand in range(1,100):
+    for page in range(1,100):
+        url = 'https://www.cars.bg/carslist.php?subm=1&add_search=1&typeoffer=1&brandId={brand}&conditions%5B%5D=4&conditions%5B%5D=1&ajax=1&page={id}'                                                               
+        print(url.format(id = page,brand = brand))
+        driver.get(url.format(id = page,brand = brand))
+        print()
+        if(len(driver.page_source)<200):
+            break
+        time.sleep(0.1)
+        file = open('DS.html', 'a', encoding="utf-8")
+        file.write(driver.page_source)
    
 file.close()
 driver.close()
--- webscraper.py
+++ webscraper.py
@@ -7,34 +7,35 @@ from selenium import webdriver
 
 #exec(open('html_download.py').read())
 
-
+# Define headers
 headers = ({'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit\
 /537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'})
 
+# Define base url and offer url 
 base_url = "https://www.cars.bg/"
 car_offer_url = "https://www.cars.bg/offer/"
 #response = get(base_url,headers=headers)
+
+# Get response from file and cook it into soup
 response_from_file = open('DS.html','r', encoding="utf-8")
 html_soup = BeautifulSoup(response_from_file,'html.parser')
 
-
+# Array holding a div card with the cars info
 basic_info =[]
 content_list = html_soup.find_all('div',attrs={'class': 'mdc-card offer-item'})
 
+# Append only divs with cars to basic_info
+for item in content_list:
+    basic_info.append(item.find_all('div', attrs={'class': 'mdc-card__primary-action'}))
 
-
+# Get ids of car from div
 def get_ids(content_list):
     ids = [] 
     for item in content_list:
         ids.append(item.attrs['data-reference'])
     return ids
-        
 
-
-for item in content_list:
-    basic_info.append(item.find_all('div', attrs={'class': 'mdc-card__primary-action'}))
-
-
+# Get names of car from div
 def get_names(basic_info):
     names = [] 
     for item in basic_info:
@@ -42,6 +43,7 @@ def get_names(basic_info):
             names.append(i.find_all("h5",attrs = {"class": "observable"})[0].text.strip())
     return names
 
+# Get prices of car from div
 def get_prices(basic_info):
     prices = [] 
     for item in basic_info:
@@ -49,7 +51,7 @@ def get_prices(basic_info):
             prices.append(i.find_all("h6",attrs = {"class": "price"})[0].text.strip())
     return prices
 
-
+# Get details of car from div
 def get_details(basic_info):
     details = [] 
     for item in basic_info:
@@ -57,26 +59,31 @@ def get_details(basic_info):
             details.append(i.find_all("div",attrs = {"class": "mdc-typography--body1"})[0].text.strip())
     return details
 
-
+# Define dataset structure
 data = {"id" :[],"brand": [],"model":[] , "year": [],
 "fuel": [], "kms":[], "price": [],"transmission":[]}
-
 df = pd.DataFrame(data)
 
 
-
+#TODO Fix item[2] not having kms or fuel
 for item in zip(get_names(basic_info),get_prices(basic_info),get_details(basic_info),get_ids(content_list)):
     id = item[3]
     brand = item[0].split()[0]
     model = item[0].split()[1]
+    print('asd')
     if(item[1] == "цена по договаряне"):
         continue
+
     price = int(item[1].split()[0].replace(",",""))
     year = item[2].split()[0].replace(",","")
     fuel = "d" if item[2].split()[1].replace(",","") == "Дизел" else "p"
-    kms = "" if len(item[2].split())<3 else int(item[2].split()[2].replace(",",""))
+    print(item)
+    if len(item[2].split()) == 2 :
+        kms = ""
+    else:
+        kms = "" if len(item[2].split())<3 else int(item[2].split()[2].replace(",","").replace("км.",""))
     row = pd.DataFrame({"id":[id],"brand": [brand],"model":[model] , "year": [year],"fuel": [fuel], "kms":[kms], "price": [price]})
-    #print(row)
+    
     df = pd.concat([df, row])
     
 df = df.drop_duplicates()
