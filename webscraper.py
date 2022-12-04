df44af2215918fd2175ab5e48361c8bd959d9031
Mario Markov <mario11septemvri@gmail.com>
2022-12-04 15:16:56 +0200

    improoved webscraping ⚡️

--- webscraper.py
+++ webscraper.py
@@ -1,25 +1,43 @@
 from unicodedata import name
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 from requests import get
 import pandas as pd
 from selenium import webdriver
-
-
+import lxml
+import cchardet
+# import OS module
+import os
 #exec(open('html_download.py').read())
 
-# Define headers
-headers = ({'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit\
-/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'})
 
-# Define base url and offer url 
-base_url = "https://www.cars.bg/"
-car_offer_url = "https://www.cars.bg/offer/"
-#response = get(base_url,headers=headers)
 
+# Get the list of all files and directories
+path = "./html_files"
+
+files = os.listdir(path)
+
+
+
+# with open('all_html.html','a',encoding='UTF-8') as all_html:
+#     for file in files:
+#         with open(path + '/' + file, "r", encoding='utf-8-sig') as single_file:
+#             html_as_string = single_file.read()
+#             all_html.write(html_as_string)
+#         single_file.close()
+
+
+# all_html.close()
+        
+
+
+
+response_from_file = open('all_html.html','r', encoding="utf-8")
+
+strainer = SoupStrainer('div', attrs={'class': 'mdc-card offer-item'})
 # Get response from file and cook it into soup
-response_from_file = open('DS.html','r', encoding="utf-8")
-html_soup = BeautifulSoup(response_from_file,'html.parser')
+html_soup = BeautifulSoup(response_from_file,'html.parser', parse_only = strainer)
 
+print("success soup")
 # Array holding a div card with the cars info
 basic_info =[]
 content_list = html_soup.find_all('div',attrs={'class': 'mdc-card offer-item'})
@@ -67,21 +85,45 @@ df = pd.DataFrame(data)
 
 #TODO Fix item[2] not having kms or fuel
 for item in zip(get_names(basic_info),get_prices(basic_info),get_details(basic_info),get_ids(content_list)):
+    print(item)
+
+    #(Opel,Astra), (20 000), (2005, diesel,200 000 km), (id)
+    #('Hyundai Terracan 2.9, CDI', '7,300 лв.', '2002, Дизел, 168350 км.', '633d762fe4515d92cd0d8103')
+
     id = item[3]
     brand = item[0].split()[0]
     model = item[0].split()[1]
-    print('asd')
+
     if(item[1] == "цена по договаряне"):
         continue
 
     price = int(item[1].split()[0].replace(",",""))
     year = item[2].split()[0].replace(",","")
-    fuel = "d" if item[2].split()[1].replace(",","") == "Дизел" else "p"
-    print(item)
-    if len(item[2].split()) == 2 :
-        kms = ""
+    
+    
+    # Determine which properties are in the [2] field 
+    # if there are only two properties
+    if len(item[2].split(', ')) == 2 :
+        second_prop = item[2].split()[1]
+        #If the second one is kms
+        if str.__contains__(second_prop, 'км'):
+            kms = int(item[2].split()[1].replace(",","").replace("км.",""))
+            fuel = "missing"
+        # if the second one is fuel 
+        else:
+            kms = "missing"
+            if item[2].split()[1].replace(",","") == 'Дизел': fuel = 'd'
+            elif item[2].split()[1].replace(",","") == 'Газ/Бензин': fuel = 'g'
+            elif item[2].split()[1].replace(",","") == 'Електричество': fuel = 'e'
+            else : fuel = 'p'
+    # if there are 3 properties
     else:
-        kms = "" if len(item[2].split())<3 else int(item[2].split()[2].replace(",","").replace("км.",""))
+        if item[2].split()[1].replace(",","") == 'Дизел': fuel = 'd'
+        elif item[2].split()[1].replace(",","") == 'Газ/Бензин': fuel = 'g'
+        elif item[2].split()[1].replace(",","") == 'Електричество': fuel = 'e'
+        else : fuel = 'p'
+        kms = int(item[2].split()[2].replace(",","").replace(" км.",""))
+
     row = pd.DataFrame({"id":[id],"brand": [brand],"model":[model] , "year": [year],"fuel": [fuel], "kms":[kms], "price": [price]})
     
     df = pd.concat([df, row])
