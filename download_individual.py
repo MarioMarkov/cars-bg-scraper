c6cca63f58209d3a3e97e44a823ef7efb67bf448
Mario Markov <mario11septemvri@gmail.com>
2022-12-28 14:41:08 +0200

    Added more features to dataset ✨

--- download_individual.py
+++ download_individual.py
@@ -61,9 +61,10 @@ for index, car in cars.iterrows():
     cars.at[index,'transmission'] = 1 if  'автоматични скорости' in details[1].text.lower() else 0
     cars.at[index,'2door'] = 1 if  '2/3 врати' in details[1].text else 0 
 
+    # Find color of car
+    matches_color = [word if word in details[1].text.lower().strip()  else None for word in color_name_en.keys()]
     
-    matches_color = [word if word in details[1].text.lower()  else None for word in color_name_en.keys()]
-    
+    # Map bulgarian names to english names if there is a match else None
     if any(matches_color):
         color = None
         for item in matches_color:
@@ -75,8 +76,10 @@ for index, car in cars.iterrows():
     else:
         cars.at[index,'color'] = None 
 
-    matches_type = [  word if word in details[1].text.lower()  else None  for word in type_of_car_en.keys()]
+    # Find type of car
+    matches_type = [  word if word in details[1].text.lower().strip()  else None  for word in type_of_car_en.keys()]
 
+    # Map bulgarian names to english names if there is a match else None
     if any(matches_type):
         type = None
         for item in matches_type:
@@ -87,9 +90,25 @@ for index, car in cars.iterrows():
     else:
         cars.at[index,'type'] = None 
 
+    # Find displacement of engine
+    dis = details[1].text.lower().find("см3")
+    dis_value = details[1].text.lower()[dis-4:dis]
+    cars.at[index,'displacement'] = dis_value if dis != -1  else None  
+
+    # Find horsepower of engine
+    hp = details[1].text.lower().find("к.с.")
+    hp_value = details[1].text.lower()[hp-3:hp]
+    cars.at[index,'hp'] = hp_value.strip().strip(',') if hp != -1  else None  
+
+    # Find euro rating
+    euro = details[1].text.lower().find("euro")
+    euro_value = details[1].text.lower()[euro+4:euro+6]
+    cars.at[index,'euro'] = euro_value.strip() if euro != -1  else None  
+
+
     print(cars.loc[cars['id'] == car.id])
 
-cars = cars[['id','brand','model','year','fuel','kms','transmission','2door','color','type','price']] 
-cars.to_csv('cars-data2.csv', index=False)
+cars = cars[['id','brand','model','year','fuel','kms','transmission','2door','color','type','displacement','hp','euro','price']] 
+cars.to_csv('cars-data3.csv', index=False)
 driver.close()  
 
