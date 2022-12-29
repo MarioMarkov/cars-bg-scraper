23c46cd17787c3218af097250e5975b245a5abfe
Mario Markov <mario11septemvri@gmail.com>
2022-12-30 00:41:49 +0200

    Improoved way of scraping car details

--- download_individual.py
+++ download_individual.py
@@ -37,6 +37,7 @@ type_of_car_en = {
     "хечбек":"hatchback",
     "комби":"combi",
     "купе": "coupe",
+    "kупе":"coupe",
     "кабрио":"cabrio",
     "джип":"jeep",
     "пикап":"pickup",
@@ -57,54 +58,55 @@ for index, car in cars.iterrows():
     if len(details) == 0:
         continue
 
+    all_details = details[1].text.lower().split(",")
+    all_details = [i.strip() for i in all_details]
+    print(all_details)
+
+
     #Add the data to the car with the car.id from cars-data.csv
-    cars.at[index,'transmission'] = 1 if  'автоматични скорости' in details[1].text.lower() else 0
-    cars.at[index,'2door'] = 1 if  '2/3 врати' in details[1].text else 0 
+    cars.at[index,'transmission'] = 1 if  'автоматични скорости' in all_details else 0
+    cars.at[index,'2door'] = 1 if  '2/3 врати' in all_details else 0 
 
     # Find color of car
-    matches_color = [word if word in details[1].text.lower().strip()  else None for word in color_name_en.keys()]
-    
+    color = None
+    for bg_color in color_name_en.keys():
+        found = [i for i in all_details if bg_color in i]
+        if found: 
+            color = bg_color
+            break
+        
     # Map bulgarian names to english names if there is a match else None
-    if any(matches_color):
-        color = None
-        for item in matches_color:
-            if item is not None: 
-                color = item
-                break
-
+    if color:
         cars.at[index,'color'] = color_name_en[color]
     else:
         cars.at[index,'color'] = None 
 
+
     # Find type of car
-    matches_type = [  word if word in details[1].text.lower().strip()  else None  for word in type_of_car_en.keys()]
+    type = None
+    for bg_type in type_of_car_en.keys():
+        found = bg_type in all_details
+        if found: 
+            type = bg_type
+            break
 
     # Map bulgarian names to english names if there is a match else None
-    if any(matches_type):
-        type = None
-        for item in matches_type:
-            if item is not None: 
-                type = item
-                break
+    if type:
         cars.at[index,'type'] = type_of_car_en[type]
     else:
         cars.at[index,'type'] = None 
 
     # Find displacement of engine
-    dis = details[1].text.lower().find("см3")
-    dis_value = details[1].text.lower()[dis-4:dis]
-    cars.at[index,'displacement'] = dis_value if dis != -1  else None  
+    dis_value = [i for i in all_details if "см3" in i]
+    cars.at[index,'displacement'] = dis_value[0].replace("см3", "").strip().strip(',') if dis_value else None  
 
     # Find horsepower of engine
-    hp = details[1].text.lower().find("к.с.")
-    hp_value = details[1].text.lower()[hp-3:hp]
-    cars.at[index,'hp'] = hp_value.strip().strip(',') if hp != -1  else None  
+    hp_value = [i for i in all_details if "к.с." in i]
+    cars.at[index,'hp'] = hp_value[0].replace("к.с.", "").strip().strip(',') if hp_value else None  
 
     # Find euro rating
-    euro = details[1].text.lower().find("euro")
-    euro_value = details[1].text.lower()[euro+4:euro+6]
-    cars.at[index,'euro'] = euro_value.strip() if euro != -1  else None  
-
+    euro_value = [i for i in all_details if "euro" in i]
+    cars.at[index,'euro'] = euro_value[0].replace("euro", "").strip().strip(',') if euro_value else None  
 
     print(cars.loc[cars['id'] == car.id])
 
